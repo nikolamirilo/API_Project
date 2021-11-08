d593d835fd6a017be59899b85bc2649e45149313
Nikola Mirilo <nikolamirilo@gmail.com>
2021-11-08 01:16:48 +0100

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Navbar, Footer } from "../components";
 
 const Error = () => {
   return (
     <div className="error">
-      <h1>Error 404</h1>
+      <Navbar />
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "40vh",
+          fontFamily: "Helvetica",
+          color: "#000000",
+          textAlign: "center",
+          fontSize: "2.5rem",
+        }}
+      >
+        Page Not Found
+      </h1>
+      <Footer />
     </div>
   );
 };
